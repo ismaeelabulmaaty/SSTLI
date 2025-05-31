3a7d26b0ff11587524778fdb69fcb6f17d1877d4
ismaeelabulmaaty <ismaeelmatty@gmail.com>
2025-05-31 10:25:16 +0300

    add migration

--- SSTLI/Data/Migrations/20250531072407_updateBatch.Designer.cs
+++ SSTLI/Data/Migrations/20250531072407_updateBatch.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SSTLI.Data;
 
@@ -10,9 +11,11 @@ using SSTLI.Data;
 namespace SSTLI.Data.Migrations
 {
     [DbContext(typeof(SstliDbContext))]
-    partial class SstliDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250531072407_updateBatch")]
+    partial class updateBatch
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SSTLI/Data/Migrations/20250531072407_updateBatch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SSTLI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateBatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EducationalQualification",
+                table: "RegistrationsAttendanceOrOnline",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Batch",
+                table: "RegistrationsAttendanceOrOnline",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EducationalQualification",
+                table: "RegistrationsAttendanceOrOnline",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Batch",
+                table: "RegistrationsAttendanceOrOnline",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
